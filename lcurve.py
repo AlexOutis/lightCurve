3ab8fccda0491be957cff5da134ef7b698c877d2
AlexOutis <alexaist4@gmail.com>
2022-09-05 23:04:16 +0200

    Sine fit for light curve (close to original)

--- lcurve.py
+++ lcurve.py
@@ -3,12 +3,12 @@ import math
 import matplotlib.pyplot as plt
 import sys
 import csv
-#from scipy.interpolate import *
+from scipy.optimize import curve_fit
 filename = 'lcurvePoints.csv'
 fields = []
 rows = []
-t0 = 0
-
+fig = plt.figure()
+ax = fig.add_subplot(111)
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
@@ -21,40 +21,32 @@ def timeconv(string):
     if (hh < 12): hh += 24
     return hh + mm/60 + ss/3600
 
+x, y, z, mag = [np.empty(0) for i in range(4)]
 
-i = 0
-x = []
-y = []
-z = []
-mag = []
 for row in rows:
-   # print(row)
     rowstr = row[0]
     x0, y0, z0, rat  = rowstr.split()
-    x0 = timeconv(x0)
-    y0 = float(y0)
-    z0 = float(z0)
-    rat = float(rat)
-    x.append(x0), y.append(y0)                                             
-    #print(type(x0))
+    x0 = timeconv(x0); y0 = float(y0); z0 = float(z0)
+    x = np.append(x, x0); y = np.append(y, y0)                                             
     rat = z0 / y0
     mag0 = -2.5 * math.log10(rat)
-    mag.append(mag0)
+    mag = np.append(mag, mag0)
 
-x00 = x[0]
-for i in range(len(x)):
-    x[i] -= x00
-#print(x)
-mag = mag
-xnew = np.linspace(x[0], x[len(x) - 1], 300)
-model = np.poly1d(np.polyfit(x, mag, 3))
-ynew = model(xnew)
+def func(x, a, b, c, d):
+    return a * np.sin(b + c*x) + d
+
+x -= x[0]
+xnew = np.linspace(x[0], x[-1], 300)
+
+param, pcov = curve_fit(func, x, mag)
+#print(param)
+ynew = func(xnew, *param)
+plt.scatter(x, mag)
 plt.plot(xnew, ynew)
+plt.suptitle("Light curve of Vesta", fontweight='bold')
 
-plt.axvline(x=0, color = 'black', linestyle= '--')
-plt.hlines(1.382, x[0], x[len(x) - 1], color = 'grey', linestyle = '--')
-plt.axvline(x=5.7, color = 'black', linestyle= '--')
-plot = plt.scatter(x, mag)
+ax.set_title(f'Period of rotation: {round(2 * math.pi/param[2], 1)}hours')
+
 plt.ylabel('Relative magnitude')
 plt.xlabel('Time in hours')
 plt.show()
